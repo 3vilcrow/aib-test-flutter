be48d44e77fb5bebd5c3d7f2d813d20622ba77df
Carlos A. Falcon <karloalejandro9703@gmail.com>
2023-08-22 21:09:35 +0200

    agregar cached network image package, agregar imagen pokemon en pokemon cad y habilitar parametro de acceso de internet en el manifesto de android

--- lib/features/pokemon/presentation/pokemon_list/widgets/pokemon_card.dart
+++ lib/features/pokemon/presentation/pokemon_list/widgets/pokemon_card.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:aib_test/core/routing/app_router.dart';
 import 'package:aib_test/features/pokemon/domain/model/pokemon/pokemon.dart';
@@ -20,11 +21,36 @@ class PokemonCard extends StatelessWidget {
         elevation: 4,
         margin: const EdgeInsets.all(16),
         child: Padding(
-          padding: const EdgeInsets.all(16),
-          child: Column(
+          padding: const EdgeInsets.all(10),
+          child: Row(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text('Nombre: ${pokemon?.name ?? "N/A"}'),
+              SizedBox(
+                height: 40,
+                width: 40,
+                child: CachedNetworkImage(
+                  placeholder: (context, url) {
+                    return const Padding(
+                      padding: EdgeInsets.all(10.0),
+                      child: CircularProgressIndicator(
+                        strokeWidth: 1,
+                      ),
+                    );
+                  },
+                  imageBuilder: (context, imageProvider) => Container(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: imageProvider,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  imageUrl:
+                      pokemon?.sprites?.image?.pokemonImageHome?.frontDefault ??
+                          '',
+                ),
+              ),
+              Text(pokemon?.name ?? 'N/A'),
             ],
           ),
         ),
